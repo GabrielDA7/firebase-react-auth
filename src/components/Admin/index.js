1c397d9639a8427cb9995e1f3326ce09b5c61848
Gabriel <gabriel.pro.d3@gmail.com>
2019-03-16 22:26:53 +0100

    Protected Routes

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {withAuthorization} from '../Session';
+import * as ROLES from '../../constants/roles';
 
-const Admin = () => (
-    <h1>
-        Admin
-    </h1>
+const AdminPage = () => (
+    <div>
+        <h1>Admin</h1>
+        <p>
+            Restricted area! Only users with the admin role are authorized.
+        </p>
+    </div>
 );
 
-export default Admin;
+const condition = authUser =>
+    authUser && authUser.roles.includes(ROLES.ADMIN);
+
+export default withAuthorization(condition)(AdminPage);
